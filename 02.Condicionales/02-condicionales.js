d82876bf690b1280d76547d6555f538dd39ca01c
Avleo <leito.avila.sm@gmail.com>
2024-03-15 15:10:08 -0300

    add 02.

---
+++ 02.Condicionales/02-condicionales.js
@@ -0,0 +1,41 @@
+function verificarNumero(numero){
+    // Escribe un programa que tome un número como entrada y muestre "Es positivo" si el número es mayor que cero,
+    // "Es negativo" si es menor que cero y "Es cero" si es igual a cero.
+    // Tu código 👇:
+
+}
+
+
+function esBisiesto(anio){
+    // Escribe un programa que determine si un año es bisiesto o no.
+    // Un año es bisiesto si es divisible entre 4, excepto aquellos que son divisibles entre 100 pero no entre 400.
+    // Tu código 👇:
+
+}
+
+function verificarCalificacion(calificacion){
+    // Escribe un programa que determine si un estudiante aprobó o reprobó un examen.
+    // El estudiante aprueba si su calificación es mayor o igual a 60.
+    // Tu código 👇:
+
+}
+
+function esMayor(num1, num2, num3){
+    // Escribe un programa que determine el mayor de tres números dados.
+    // Tu código 👇:
+
+}
+
+function esPrimo(num){
+    // Escribe un programa que determine si un número dado es primo.
+    // Tu código 👇:
+
+}
+
+module.exports = {
+    verificarCalificacion,
+    verificarNumero,
+    esBisiesto,
+    esMayor,
+    esPrimo
+}
---
+++ __test__/02.test.js
@@ -0,0 +1,35 @@
+const {
+    esBisiesto,
+    esMayor,
+    esPrimo,
+    verificarCalificacion,
+    verificarNumero
+} = require('../02.Condicionales/02-condicionales.js')
+const { default: expect } = require('expect')
+
+describe( "esBisiesto", () =>{
+    it( "Debe devolver si un numero es Bisiesto", ()=> {
+        expect().toBe()
+    })
+})
+describe( "esMayor", () =>{
+    it( "Debe devolver el mayor de los tres numeros", ()=> {
+        expect().toBe()
+    })
+})
+describe( "esPrimo", () =>{
+    it( "Debe devolver si el numero es primo", ()=> {
+        expect().toBe()
+    })
+})
+describe( "verificarNumero", () =>{
+    it( "Debe devolver si el numero es positivo, negativo o cero", ()=> {
+        expect().toBe()
+    })
+})
+
+describe( "verificarCalificacion", () =>{
+    it( "Debe devolver Aprobo o Desaprobo", ()=> {
+        expect().toBe()
+    })
+})
