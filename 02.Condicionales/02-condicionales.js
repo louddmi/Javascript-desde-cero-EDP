911e56781a7f04ba30c6871ef88782e6cd648ef6
AvLeo <leito.avila.sm@gmail.com>
2024-03-20 02:04:34 -0300

    fixed test 02-condicionales

--- 02.Condicionales/02-condicionales.js
+++ 02.Condicionales/02-condicionales.js
@@ -7,7 +7,7 @@ function verificarNumero(numero){
 
 
 function esBisiesto(anio){
-    // Escribe un programa que determine si un año es bisiesto o no.
+    // Escribe un programa que determine si un año es bisiesto o no. (true o false)
     // Un año es bisiesto si es divisible entre 4, excepto aquellos que son divisibles entre 100 pero no entre 400.
     // Tu código 👇:
 
@@ -16,18 +16,21 @@ function esBisiesto(anio){
 function verificarCalificacion(calificacion){
     // Escribe un programa que determine si un estudiante aprobó o reprobó un examen.
     // El estudiante aprueba si su calificación es mayor o igual a 60.
+    // Devolver "Aprobó" o "Reprobó"
     // Tu código 👇:
 
 }
 
 function esMayor(num1, num2, num3){
     // Escribe un programa que determine el mayor de tres números dados.
+    // Devolver el numero mayor
     // Tu código 👇:
 
 }
 
 function esPrimo(num){
     // Escribe un programa que determine si un número dado es primo.
+    // Devolver true o false
     // Tu código 👇:
 
 }
--- __test__/02.test.js
+++ __test__/02.test.js
@@ -9,27 +9,37 @@ const { default: expect } = require('expect')
 
 describe( "esBisiesto", () =>{
     it( "Debe devolver si un numero es Bisiesto", ()=> {
-        expect().toBe()
+        expect(esBisiesto(2020)).toBe(true)
+        expect(esBisiesto(2024)).toBe(true)
+        expect(esBisiesto(2022)).toBe(false)
     })
 })
 describe( "esMayor", () =>{
     it( "Debe devolver el mayor de los tres numeros", ()=> {
-        expect().toBe()
+        expect(esMayor(10,21,99)).toEqual(99)
+        expect(esMayor(1,5,3)).toEqual(5)
+        expect(esMayor(55,40,20)).toEqual(55)
     })
 })
-describe( "esPrimo", () =>{
-    it( "Debe devolver si el numero es primo", ()=> {
-        expect().toBe()
+describe( "verificarCalificacion", () =>{
+    it( "Debe devolver Aprobó o Desaprob", ()=> {
+        expect(verificarCalificacion(80)).toBe("Aprobó")
+        expect(verificarCalificacion(50)).toBe("Reprobó")
+        expect(verificarCalificacion(60)).toBe("Aprobó")
     })
 })
 describe( "verificarNumero", () =>{
     it( "Debe devolver si el numero es positivo, negativo o cero", ()=> {
-        expect().toBe()
+        expect(verificarNumero(7)).toBe("Es positivo")
+        expect(verificarNumero(-4)).toBe("Es negativo")
+        expect(verificarNumero(0)).toBe("Es cero")
     })
 })
 
-describe( "verificarCalificacion", () =>{
-    it( "Debe devolver Aprobo o Desaprobo", ()=> {
-        expect().toBe()
+describe( "esPrimo", () =>{
+    it( "Debe devolver true si el numero es primo", ()=> {
+        expect(esPrimo(7)).toBe(true)
+        expect(esPrimo(4)).toBe(false)
+        expect(esPrimo(3)).toBe(true)
     })
 })
